55d2f697a40aa4f189924d14437d87a2b6fee84b
SimonExopi <sa@exopi.dk>
2025-10-06 14:20:20 +0200

    Initial implementation of Menu. Found under MenuService.cs Added the two following classes _standardMenu and _lunchMenu, each has the same type of food but differeate in prices, with a title indicate it is a lunch special offer.

--- Tests/PizzaPlace.Test/obj/Debug/net8.0/PizzaPlace.Test.AssemblyInfo.cs
+++ Tests/PizzaPlace.Test/obj/Debug/net8.0/PizzaPlace.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaPlace.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+554d379ce678a81ddeb7fbdd6bdc46c50574025e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8657025c9e99240e780ff32c763c9789c4d01b5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzaPlace.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzaPlace.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
